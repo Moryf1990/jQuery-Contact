e22b3bfc751677e7138e5e20036b69f5eb1eec52
Mory Friedman <friedmanmory2@gmail.com>
2016-03-21 23:12:35 -0500

    making email input require an @

--- scripts/main.js
+++ scripts/main.js
@@ -16,24 +16,35 @@ button.on('click', function() {
 	   hasError=true;
 	   error1.html('Name cannot be left empty');
 	   error1.css({marginBottom: '1em'});
+	   nameInput.css({borderLeft: '0.5em solid red'});
 	}
 
-	if(emailInput.val()==='') {
+	if(emailInput.val() ==='') {
+		hasError=true;
+		error2.html('Please enter an email address');
+		error2.css({marginBottom: '1em'});
+		emailInput.css({borderLeft: '0.5em solid red'});
+	}
+
+	if(emailInput.val() !=='' && emailInput.val().indexOf('@') === -1) {
 		hasError=true;
 		error2.html('Email must have contain an \'@\'');
 		error2.css({marginBottom: '1em'});
+		emailInput.css({borderLeft: '0.5em solid red'});
 	}
 
 	if(websiteInput.val()==='') {
 		hasError=true;
 		error3.html('Website must start with http://');
 		error3.css({marginBottom: '1em'});
+		websiteInput.css({borderLeft: '0.5em solid red'});
 	}
 
 	if(textarea.val()==='') {
 		hasError=true;
 		error4.html('Message cannot be left empty');
 		error4.css({marginBottom: '1em'});
+		textarea.css({borderLeft: '0.5em solid red'});
 	}
 
 	if(!hasError===true){
